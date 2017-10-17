07ec814e4aafe221e25ca1a5c0e26fb8adaa6cd6
Andrew Bellucci <abellucci@email.neit.edu>
2017-10-17 11:19:39 -0400

    added files and start project

---
+++ lab2/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Lab 2 | PHP</title>
+    <link rel="stylesheet" type="text/css" href="assets/style.css">
+</head>
+<body>
+    <?php
+        include_once 'assets/nav.html';
+    ?>
+    <div class="wrapper">
+        <div class="section">
+            <h1 class="page-title">Actors</h1>
+            <?php
+                
+            ?>
+        </div>
+    </div>
+</body>
+</html>
---
+++ wk3demo/assets/dogform.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bellu
+ * Date: 10/16/2017
+ * Time: 1:15 PM
+ */?>
+<form method="post" action="#">
+    Name: <input type="text" name="name"><br>
+    Gender: Male: <input type="radio" name="gender" value="M">Female: <input type="radio" name="gender" value="F"><br>
+    Fixed: <input type="checkbox" name="fixed" value="true"><br>
+    <input type="submit" name="action" value="Add">
+</form>
